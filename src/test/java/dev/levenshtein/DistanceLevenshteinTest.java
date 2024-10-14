e432bb3b57eaa1621510f5e969d8f769f5b4cd4a
Félix BERNARD <felix.bernard@epsi.fr>
2024-10-14 15:11:54 +0200

    TP 1.3

--- src/test/java/dev/levenshtein/DistanceLevenshteinTest.java
+++ src/test/java/dev/levenshtein/DistanceLevenshteinTest.java
@@ -1,49 +1,23 @@
 package dev.levenshtein;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 /**
- * Classe de test pour DistanceLevenshtein.
+ * Classe de test pour DistanceLevenshtein utilisant un fichier CSV.
  */
 public class DistanceLevenshteinTest {
 
     // Attribut pour l'instance de DistanceLevenshtein
-    private DistanceLevenshtein distanceLevenshtein;
+    private DistanceLevenshtein distanceLevenshtein = new DistanceLevenshtein();
 
-    // Tableau statique contenant les cas de test (mot1, mot2, distance attendue)
-    private static Object[][] casDeTest;
-
-    // Méthode exécutée une seule fois avant tous les tests pour initialiser le tableau de test
-    @BeforeEach
-    public void setUp() {
-        distanceLevenshtein = new DistanceLevenshtein();
-
-        // Initialisation du tableau de cas de test
-        casDeTest = new Object[][]{
-                {"chat", "chats", 1},
-                {"machins", "machine", 1},
-                {"java", "jee", 3}
-        };
-    }
-
-    // Méthode de test pour le premier cas (chat et chats)
-    @Test
-    public void testCalculerDistanceChatChats() {
-        assertEquals(casDeTest[0][2], distanceLevenshtein.calculerDistance((String) casDeTest[0][0], (String) casDeTest[0][1]));
-    }
-
-    // Méthode de test pour le deuxième cas (machins et machine)
-    @Test
-    public void testCalculerDistanceMachinsMachine() {
-        assertEquals(casDeTest[1][2], distanceLevenshtein.calculerDistance((String) casDeTest[1][0], (String) casDeTest[1][1]));
-    }
-
-    // Méthode de test pour le troisième cas (java et jee)
-    @Test
-    public void testCalculerDistanceJavaJee() {
-        assertEquals(casDeTest[2][2], distanceLevenshtein.calculerDistance((String) casDeTest[2][0], (String) casDeTest[2][1]));
+    // Test paramétré utilisant le fichier CSV
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dev/levenshtein/DistanceLevenshtein.csv", numLinesToSkip = 1)
+    public void calculerDistance(String mot1, String mot2, int distanceAttendue) {
+        Assertions.assertEquals(distanceAttendue, distanceLevenshtein.calculerDistance(mot1, mot2));
     }
 }
